d352b37a0d4337837ca157a57075bdf26cf687e1
YrecPodlec <domofon57@gmail.com>
2026-04-13 10:25:59 +0300

    language

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,31 @@
+import { NextIntlClientProvider } from "next-intl";
+import { routing } from "@/i18n/routing";
+import { hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { getMessages } from "next-intl/server";
+import React from "react";
+
+type Props = {
+    children: React.ReactNode;
+    params: Promise<{ locale: string }>;
+};
+
+export default async function RootLayout({ children, params }: Props) {
+    const { locale } = await params;
+
+    if (!hasLocale(routing.locales, locale)) {
+        notFound();
+    }
+
+    const messages = await getMessages();
+
+    return (
+        <html lang={locale}>
+        <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+            {children}
+        </NextIntlClientProvider>
+        </body>
+        </html>
+    );
+}
