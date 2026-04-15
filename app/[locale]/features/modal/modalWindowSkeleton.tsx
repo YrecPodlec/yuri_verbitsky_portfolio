53c10b4d322a66188bf8dd066fd0bc069d191afa
YrecPodlec <domofon57@gmail.com>
2026-04-15 12:17:53 +0300

    modal window styles

--- app/[locale]/features/modal/modalWindowSkeleton.tsx
+++ app/[locale]/features/modal/modalWindowSkeleton.tsx
@@ -19,7 +19,20 @@ const ModalWindowSkeleton = ({ open, onClose, children }: Props) => {
     return (
         <section className={`${styles.overlay} ${open ? styles.open : ""}`} onClick={onClose}>
             <div className={`${styles.modal} ${open ? styles.open : ""}`} onClick={(e) => e.stopPropagation()}>
-                {children}
+                <div className="window" style={{height:'100%', display:'flex', flexDirection:'column'}}>
+                        <div className={styles.modalBar}>
+                            <div className="title-bar-text">Modal Window</div>
+                            <button className={styles.closeBtn} onClick={onClose}>X</button>
+                        </div>
+                    <div className="window-body" style={{flex: 1, overflow: 'auto', minHeight: 0, margin: 0, padding: ".5rem"}}>
+                        {children}
+                    </div>
+                    <div className="status-bar">
+                        <p className="status-bar-field" style={{fontSize: ".5rem"}}>Press F1 for help</p>
+                        <p className="status-bar-field" style={{fontSize: ".5rem"}}>Slide 1</p>
+                        <p className="status-bar-field" style={{fontSize: ".5rem"}}>CPU Usage: 14%</p>
+                    </div>
+                </div>
             </div>
         </section>
     );
