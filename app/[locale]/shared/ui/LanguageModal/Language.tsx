15bcbb26a19dba2bb4617da8e6028c8282a3a32c
YrecPodlec <domofon57@gmail.com>
2026-04-16 14:50:36 +0300

    lang flag btn

--- app/[locale]/shared/ui/LanguageModal/Language.tsx
+++ app/[locale]/shared/ui/LanguageModal/Language.tsx
@@ -1,11 +1,22 @@
+"use client"
 import React from 'react';
-
+import {useLocale} from "use-intl";
+import {LOCALES} from "@/app/[locale]/shared/i18n/locales";
+import Image from "next/image";
+import styles from './language.module.scss'
 const Language = () => {
+    const locale = useLocale();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const flag = LOCALES[locale];
     return (
-        <div className='window' style={{cursor: 'pointer'}}>
-            <div className='field-border' style={{padding: ".5rem"}}>
-                LANGUAGE
-            </div>
+        <div style={{cursor: 'pointer'}}>
+            <button className={`${styles.language}`}>
+                <p>
+                    LANGUAGE
+                </p>
+                <Image src={`/lang/${flag.flag}.svg`} width={32} height={24} alt={flag.flag} />
+            </button>
         </div>
     );
 };
